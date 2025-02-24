023164fd0459ee93561963f0055735edcdfcbe80
Jiraphorn4625 <65010914625@msu.ac.th>
2025-02-25 01:49:15 +0700

    s

--- admin/admin/back.php
+++ admin/admin/back.php
@@ -688,7 +688,6 @@
                 </footer>
             </div>
         </div>
-
         <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="js/scripts.js"></script>
