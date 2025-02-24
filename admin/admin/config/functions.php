44f97932054005170e7b5b3320c19cc01022ba09
Jiraphorn4625 <65010914625@msu.ac.th>
2025-02-25 01:26:44 +0700

    s

--- admin/admin/config/functions.php
+++ admin/admin/config/functions.php
@@ -1,8 +1,8 @@
 <?php
 // Database configuration
-$host = 'http://45.154.26.121/phpmyadmin/index.php'; // or your database host
+$host = ''; // or your database host
 $dbname = 'ElitePixel';
-$username = 'admin_user';
+$username = 'root';
 $password = 'qq123456';
 
 try {
