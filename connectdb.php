e5296ad677d29552d423c5f25bd2b4981f3eec96
Jiraphorn4625 <65010914625@msu.ac.th>
2025-03-02 21:15:06 +0700

    s

--- connectdb.php
+++ connectdb.php
@@ -8,8 +8,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 
 // Check connection
-if ($conn->connect_error) {
-  die("เชื่อมต่อไม่สำเร็จ: " . $conn->connect_error);
-}
-echo "เชื่อมต่อสำเร็จ";
+//if ($conn->connect_error) { die("เชื่อมต่อไม่สำเร็จ: " . $conn->connect_error);}
+//echo "เชื่อมต่อสำเร็จ";
 ?>
