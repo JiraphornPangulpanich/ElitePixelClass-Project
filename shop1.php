911305f699b829310ca6e69074f11011fba13d6c
Jiraphorn4625 <65010914625@msu.ac.th>
2025-03-03 00:44:09 +0700

    s

--- shop1.php
+++ shop1.php
@@ -216,6 +216,48 @@ $sql = "SELECT * FROM 'Product' ORDER BY 'Iditem' ASC";
 $rs = mysqli_query($conn , $sql);
 ?>
 
+div class="container-fluid pt-5">
+    <div class="text-center mb-4">
+        <h2 class="section-title px-5"><span class="px-2">Trendy Products</span></h2>
+    </div>
+    <div class="row px-xl-5">
+        <?php
+        while ($data = mysqli_fetch_array($rs)){
+
+        ?>
+            <!-- เรียงสินค้าในแต่ละคอลัมน์ -->
+            <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
+                <div class="card product-item border-0 mb-4">
+                    <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
+                        <!-- แสดงรูปภาพจากฐานข้อมูล -->
+                        <img 
+                             src="img/trendy/<?php echo $data['p_id']; ?>.<?php echo $data['p_ext']; ?>" 
+                            alt="<?php echo $data['p_name']; ?>" 
+                            class="img-fluid w-100"
+                            style="max-height: 300px; object-fit: cover; border-radius: 5px;"
+                        >
+                    </div>
+                    <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
+                        <!-- แสดงชื่อสินค้า -->
+                        <h6 class="text-truncate mb-3"><?php echo $data['p_name']; ?></h6>
+                        <div class="d-flex justify-content-center">
+                            <!-- แสดงราคา -->
+                            <h6>฿<?php echo $data['p_price']; ?></h6>
+                        </div>
+                    </div>
+                    <div class="card-footer d-flex justify-content-between bg-light border">
+                        <a href="detail.php?p_id=<?php echo $data['p_id']; ?>" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-primary mr-1"></i>View Detail</a>
+                        <a href="" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart</a>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+</div>
+
+
+
+
             <!-- Shop Product Start -->
             <div class="col-lg-9 col-md-8">
                 <div class="row pb-3">
