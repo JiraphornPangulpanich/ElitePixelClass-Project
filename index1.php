2c2735f3356856b6c5fdd51329e31eacba962b88
Jiraphorn4625 <65010914625@msu.ac.th>
2025-03-02 21:30:23 +0700

    s

--- index1.php
+++ index1.php
@@ -558,7 +558,7 @@ $rs = mysqli_query($conn , $sql);
                     <div class="text-center py-4">
                         <a class="h6 text-decoration-none text-truncate" href="">
                             <?php
-                                $sql = "SELECT Iditem FROM Product WHERE Iditem = '301'";
+                                $sql = "SELECT Iditem , Name FROM Product WHERE Iditem = '301'";
                                 $result = $conn->query($sql);
 
                                 if ($result->num_rows > 0) {
