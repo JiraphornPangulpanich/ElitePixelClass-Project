367568696e8b8011375b6fff31f2c6418342776b
Jiraphorn4625 <65010914625@msu.ac.th>
2025-03-03 00:44:30 +0700

    s

--- shop1.php
+++ shop1.php
@@ -216,7 +216,7 @@ $sql = "SELECT * FROM 'Product' ORDER BY 'Iditem' ASC";
 $rs = mysqli_query($conn , $sql);
 ?>
 
-div class="container-fluid pt-5">
+<div class="container-fluid pt-5">
     <div class="text-center mb-4">
         <h2 class="section-title px-5"><span class="px-2">Trendy Products</span></h2>
     </div>
