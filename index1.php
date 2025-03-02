277324724c3735bade9d9cc8ce7fb5c20e802e8b
Jiraphorn4625 <65010914625@msu.ac.th>
2025-03-02 22:33:02 +0700

    s

--- index1.php
+++ index1.php
@@ -804,25 +804,65 @@ $rs = mysqli_query($conn , $sql);
             <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
                 <div class="product-item bg-light mb-4">
                     <div class="product-img position-relative overflow-hidden">
-                        <img class="img-fluid w-100" src="img/sc1-1.jpg" alt="">
+                        <img class="img-fluid w-100" src="img/402.1.jpg" alt="">
                         <div class="product-action">
                             <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-shopping-cart"></i></a>
-                            <a class="btn btn-outline-dark btn-square" href=""><i class="far fa-heart"></i></a>
-                            <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-sync-alt"></i></a>
                             <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-search"></i></a>
                         </div>
                     </div>
                     <div class="text-center py-4">
-                        <a class="h6 text-decoration-none text-truncate" href="">LG 24MR400-B FHD 3-Side Borderless</a>
+                    <a class="h6 text-decoration-none text-truncate" href="">
+                            <?php
+                                $sql = "SELECT Iditem , Name FROM Product WHERE Iditem = '402'";
+                                $result = $conn->query($sql);
+
+                                if ($result->num_rows > 0) {
+                                while ($row = $result->fetch_assoc()) {
+                                echo $row["Name"] . "<br>";
+                                }
+                                } else {
+                                    echo "ไม่พบข้อมูล";
+                                }
+                            ?>
+                        </a>
                         <div class="d-flex align-items-center justify-content-center mt-2">
-                            <h5>$2,800.00</h5><h6 class="text-muted ml-2"><del>$3,000.00</del></h6>
+                            <h5 class="text-muted ml-2">$<?php
+                                $sql = "SELECT Iditem, Price FROM Product WHERE Iditem = '402'";
+                                $result = $conn->query($sql);
+
+                                if ($result->num_rows > 0) {
+                                while ($row = $result->fetch_assoc()) {
+                                echo $row["Price"] . "<br>";
+                                }
+                                } else {
+                                    echo "ไม่พบข้อมูล";
+                                }
+                                ?>
+                            </h5><h6 class="text-muted ml-2"><del>$10,000.00</del></h6>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-center mt-2">
+                            <h6 style="font-size: 12px; ">เหลือ &nbsp <h6>
+                            <h6 style="font-size: 12px; ">
+                                <?php
+                                    $sql = "SELECT Iditem, Num FROM Product WHERE Iditem = '402'";
+                                    $result = $conn->query($sql);
+
+                                    if ($result->num_rows > 0) {
+                                    while ($row = $result->fetch_assoc()) {
+                                    echo $row["Num"] . "<br>";
+                                    }
+                                    } else {
+                                        echo "ไม่พบข้อมูล";
+                                    }
+                                ?></h6>
+                            <h6 style="font-size: 12px; ">&nbsp  ชิ้น</h6>
                         </div>
                         <div class="d-flex align-items-center justify-content-center mb-1">
                             <small class="fa fa-star text-primary mr-1"></small>
                             <small class="fa fa-star text-primary mr-1"></small>
                             <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star-half-alt text-primary mr-1"></small>
+                            <small class="far fa-star text-primary mr-1"></small>
+                            <small class="far fa-star text-primary mr-1"></small>
                             <small>(99)</small>
                         </div>
                     </div>
@@ -898,18 +938,58 @@ $rs = mysqli_query($conn , $sql);
             <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
                 <div class="product-item bg-light mb-4">
                     <div class="product-img position-relative overflow-hidden">
-                        <img class="img-fluid w-100" src="img/h1-1.jpg" alt="">
+                        <img class="img-fluid w-100" src="img/701.1.jpg" alt="">
                         <div class="product-action">
                             <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-shopping-cart"></i></a>
-                            <a class="btn btn-outline-dark btn-square" href=""><i class="far fa-heart"></i></a>
-                            <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-sync-alt"></i></a>
                             <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-search"></i></a>
                         </div>
                     </div>
                     <div class="text-center py-4">
-                        <a class="h6 text-decoration-none text-truncate" href="">Marshall Monitor III A.N.C</a>
+                    <a class="h6 text-decoration-none text-truncate" href="">
+                            <?php
+                                $sql = "SELECT Iditem , Name FROM Product WHERE Iditem = '701'";
+                                $result = $conn->query($sql);
+
+                                if ($result->num_rows > 0) {
+                                while ($row = $result->fetch_assoc()) {
+                                echo $row["Name"] . "<br>";
+                                }
+                                } else {
+                                    echo "ไม่พบข้อมูล";
+                                }
+                            ?>
+                        </a>
                         <div class="d-flex align-items-center justify-content-center mt-2">
-                            <h5>$12,990.00</h5><h6 class="text-muted ml-2"><del>$13,000.00</del></h6>
+                            <h5 class="text-muted ml-2">$<?php
+                                $sql = "SELECT Iditem, Price FROM Product WHERE Iditem = '701'";
+                                $result = $conn->query($sql);
+
+                                if ($result->num_rows > 0) {
+                                while ($row = $result->fetch_assoc()) {
+                                echo $row["Price"] . "<br>";
+                                }
+                                } else {
+                                    echo "ไม่พบข้อมูล";
+                                }
+                                ?>
+                            </h5><h6 class="text-muted ml-2"><del>$30,000.00</del></h6>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-center mt-2">
+                            <h6 style="font-size: 12px; ">เหลือ &nbsp <h6>
+                            <h6 style="font-size: 12px; ">
+                                <?php
+                                    $sql = "SELECT Iditem, Num FROM Product WHERE Iditem = '701'";
+                                    $result = $conn->query($sql);
+
+                                    if ($result->num_rows > 0) {
+                                    while ($row = $result->fetch_assoc()) {
+                                    echo $row["Num"] . "<br>";
+                                    }
+                                    } else {
+                                        echo "ไม่พบข้อมูล";
+                                    }
+                                ?></h6>
+                            <h6 style="font-size: 12px; ">&nbsp  ชิ้น</h6>
                         </div>
                         <div class="d-flex align-items-center justify-content-center mb-1">
                             <small class="fa fa-star text-primary mr-1"></small>
@@ -925,25 +1005,65 @@ $rs = mysqli_query($conn , $sql);
             <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
                 <div class="product-item bg-light mb-4">
                     <div class="product-img position-relative overflow-hidden">
-                        <img class="img-fluid w-100" src="img/s2-1.jpg" alt="">
+                        <img class="img-fluid w-100" src="img/702.1.jpg" alt="">
                         <div class="product-action">
                             <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-shopping-cart"></i></a>
-                            <a class="btn btn-outline-dark btn-square" href=""><i class="far fa-heart"></i></a>
-                            <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-sync-alt"></i></a>
                             <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-search"></i></a>
                         </div>
                     </div>
                     <div class="text-center py-4">
-                        <a class="h6 text-decoration-none text-truncate" href="">JBL PartyBox Encore Essential</a>
+                    <a class="h6 text-decoration-none text-truncate" href="">
+                            <?php
+                                $sql = "SELECT Iditem , Name FROM Product WHERE Iditem = '702'";
+                                $result = $conn->query($sql);
+
+                                if ($result->num_rows > 0) {
+                                while ($row = $result->fetch_assoc()) {
+                                echo $row["Name"] . "<br>";
+                                }
+                                } else {
+                                    echo "ไม่พบข้อมูล";
+                                }
+                            ?>
+                        </a>
                         <div class="d-flex align-items-center justify-content-center mt-2">
-                            <h5>$12,900.00</h5><h6 class="text-muted ml-2"><del>$13,000.00</del></h6>
+                            <h5 class="text-muted ml-2">$<?php
+                                $sql = "SELECT Iditem, Price FROM Product WHERE Iditem = '702'";
+                                $result = $conn->query($sql);
+
+                                if ($result->num_rows > 0) {
+                                while ($row = $result->fetch_assoc()) {
+                                echo $row["Price"] . "<br>";
+                                }
+                                } else {
+                                    echo "ไม่พบข้อมูล";
+                                }
+                                ?>
+                            </h5><h6 class="text-muted ml-2"><del>$13,500.00</del></h6>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-center mt-2">
+                            <h6 style="font-size: 12px; ">เหลือ &nbsp <h6>
+                            <h6 style="font-size: 12px; ">
+                                <?php
+                                    $sql = "SELECT Iditem, Num FROM Product WHERE Iditem = '401'";
+                                    $result = $conn->query($sql);
+
+                                    if ($result->num_rows > 0) {
+                                    while ($row = $result->fetch_assoc()) {
+                                    echo $row["Num"] . "<br>";
+                                    }
+                                    } else {
+                                        echo "ไม่พบข้อมูล";
+                                    }
+                                ?></h6>
+                            <h6 style="font-size: 12px; ">&nbsp  ชิ้น</h6>
                         </div>
                         <div class="d-flex align-items-center justify-content-center mb-1">
                             <small class="fa fa-star text-primary mr-1"></small>
                             <small class="fa fa-star text-primary mr-1"></small>
                             <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
+                            <small class="far fa-star text-primary mr-1"></small>
+                            <small class="far fa-star text-primary mr-1"></small>
                             <small>(99)</small>
                         </div>
                     </div>
@@ -952,25 +1072,65 @@ $rs = mysqli_query($conn , $sql);
             <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
                 <div class="product-item bg-light mb-4">
                     <div class="product-img position-relative overflow-hidden">
-                        <img class="img-fluid w-100" src="img/sc2-1.jpg" alt="">
+                        <img class="img-fluid w-100" src="img/602.1.jpg" alt="">
                         <div class="product-action">
                             <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-shopping-cart"></i></a>
-                            <a class="btn btn-outline-dark btn-square" href=""><i class="far fa-heart"></i></a>
-                            <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-sync-alt"></i></a>
                             <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-search"></i></a>
                         </div>
                     </div>
                     <div class="text-center py-4">
-                        <a class="h6 text-decoration-none text-truncate" href="">MSI PRO MP251</a>
+                    <a class="h6 text-decoration-none text-truncate" href="">
+                            <?php
+                                $sql = "SELECT Iditem , Name FROM Product WHERE Iditem = '602'";
+                                $result = $conn->query($sql);
+
+                                if ($result->num_rows > 0) {
+                                while ($row = $result->fetch_assoc()) {
+                                echo $row["Name"] . "<br>";
+                                }
+                                } else {
+                                    echo "ไม่พบข้อมูล";
+                                }
+                            ?>
+                        </a>
                         <div class="d-flex align-items-center justify-content-center mt-2">
-                            <h5>$2,800.00</h5><h6 class="text-muted ml-2"><del>$2,850.00</del></h6>
+                            <h5 class="text-muted ml-2">$<?php
+                                $sql = "SELECT Iditem, Price FROM Product WHERE Iditem = '602'";
+                                $result = $conn->query($sql);
+
+                                if ($result->num_rows > 0) {
+                                while ($row = $result->fetch_assoc()) {
+                                echo $row["Price"] . "<br>";
+                                }
+                                } else {
+                                    echo "ไม่พบข้อมูล";
+                                }
+                                ?>
+                            </h5><h6 class="text-muted ml-2"><del>$750.00</del></h6>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-center mt-2">
+                            <h6 style="font-size: 12px; ">เหลือ &nbsp <h6>
+                            <h6 style="font-size: 12px; ">
+                                <?php
+                                    $sql = "SELECT Iditem, Num FROM Product WHERE Iditem = '602'";
+                                    $result = $conn->query($sql);
+
+                                    if ($result->num_rows > 0) {
+                                    while ($row = $result->fetch_assoc()) {
+                                    echo $row["Num"] . "<br>";
+                                    }
+                                    } else {
+                                        echo "ไม่พบข้อมูล";
+                                    }
+                                ?></h6>
+                            <h6 style="font-size: 12px; ">&nbsp  ชิ้น</h6>
                         </div>
                         <div class="d-flex align-items-center justify-content-center mb-1">
                             <small class="fa fa-star text-primary mr-1"></small>
                             <small class="fa fa-star text-primary mr-1"></small>
                             <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star text-primary mr-1"></small>
-                            <small class="fa fa-star-half-alt text-primary mr-1"></small>
+                            <small class="far fa-star text-primary mr-1"></small>
+                            <small class="far fa-star text-primary mr-1"></small>
                             <small>(99)</small>
                         </div>
                     </div>
