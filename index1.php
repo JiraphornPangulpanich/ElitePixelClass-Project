98df911887232e326b24082ab09144e0a5e5eac3
Jiraphorn4625 <65010914625@msu.ac.th>
2025-03-02 20:53:39 +0700

    s

--- index1.php
+++ index1.php
@@ -471,7 +471,7 @@ $rs = mysqli_query($conn , $sql);
                             ?></h5><h6 class="text-muted ml-2"><del>$4,500.00</del></h6>
                         </div>
                         <div class="d-flex align-items-center justify-content-center mt-2">
-                            <h6 >เหลือ</h6>
+                            <p>เหลือ</p>
                             <?php
                                 $sql = "SELECT Iditem, Num FROM Product WHERE Name = ' Gconic A98 Ultra'";
                                 $result = $conn->query($sql);
@@ -483,7 +483,7 @@ $rs = mysqli_query($conn , $sql);
                                 } else {
                                     echo "ไม่พบข้อมูล";
                                 }
-                            ?><h6>ชิ้น</h6>
+                            ?><p>ชิ้น</p>
                         </div>
                         <div class="d-flex align-items-center justify-content-center mb-1">
                             <small class="fa fa-star text-primary mr-1"></small>
