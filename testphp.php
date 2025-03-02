05f6e96f13184c8ef9e7ab3536603ef6dee94143
Jiraphorn4625 <65010914625@msu.ac.th>
2025-03-02 20:13:23 +0700

    s

--- testphp.php
+++ testphp.php
@@ -7,6 +7,12 @@ $dbname = "ElitePixel";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+if ($conn->connect_error) {
+    die("เชื่อมต่อไม่สำเร็จ: " . $conn->connect_error);
+  }
+  echo "เชื่อมต่อสำเร็จ";
+
+
 // ตรวจสอบการเชื่อมต่อ
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
