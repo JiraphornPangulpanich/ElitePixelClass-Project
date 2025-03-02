9dabc8e2c3d7bac6605ba8543f36a7c1ad46c2d4
Jiraphorn4625 <65010914625@msu.ac.th>
2025-03-02 23:31:10 +0700

    s

--- detail1.php
+++ detail1.php
@@ -243,7 +243,7 @@
                         <small class="pt-1">(99 Reviews)</small>
                     </div>
                 <h3 class="font-weight-semi-bold mb-4">฿<?php echo $product['Price']; ?></h3>
-                <p class="mb-4"><?php echo $product['Detail']; ?></p>
+                <p class="mb-4" style="font-size: 12px;"><?php echo $product['Detail']; ?></p>
 
             
             
