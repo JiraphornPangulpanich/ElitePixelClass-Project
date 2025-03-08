eeb34b3e8ef15617c0b772481aa1ab3caf6c6568
Jiraphorn4625 <65010914625@msu.ac.th>
2025-03-08 17:07:45 +0000

    a

--- index1.php
+++ index1.php
@@ -47,8 +47,9 @@
                     <a class="text-body mr-3" href="Team1.php">Team</a>
                         <button type="button" class="btn btn-sm btn-light dropdown-toggle" data-toggle="dropdown">My Account</button>
                         <div class="dropdown-menu dropdown-menu-right">
-                            <button class="dropdown-item" type="button" >Sign in</button>
-                            <button class="dropdown-item" type="button" >Logout</button>
+                        <a class="dropdown-item" href="index.php">Sign in</a>
+                        <a class="dropdown-item" href="logout.php">Logout</a>
+
                         </div>
                     </div>
                     
