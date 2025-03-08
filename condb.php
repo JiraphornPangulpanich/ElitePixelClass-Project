8e2294018eca9dc67c6cffffad6ceb48e515313c
Jiraphorn4625 <65010914625@msu.ac.th>
2025-03-08 15:10:54 +0000

    a

--- condb.php
+++ condb.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "qq123456";
-$dbname = "member"
+$dbname = "ElitePixel"
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password,$dbname );
