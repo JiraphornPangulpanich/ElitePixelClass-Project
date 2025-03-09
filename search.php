e31897b6594d7071f91a3392b67f6222a33d0ebc
Jiraphorn4625 <65010914625@msu.ac.th>
2025-03-09 22:54:42 +0700

    ห

--- search.php
+++ search.php
@@ -174,61 +174,57 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
     <!-- Navbar Start -->
-    <div class="container-fluid bg-dark mb-30">
-        <div class="row px-xl-5">
-            <div class="col-lg-3 d-none d-lg-block">
-                <a class="btn d-flex align-items-center justify-content-between bg-primary w-100" data-toggle="collapse" href="#navbar-vertical" style="height: 65px; padding: 0 30px;">
-                    <h6 class="text-dark m-0"><i class="fa fa-bars mr-2"></i>Categories</h6>
-                    <i class="fa fa-angle-down text-dark"></i>
+<div class="container-fluid bg-dark mb-30">
+    <div class="row px-xl-5">
+        <div class="col-lg-3 d-none d-lg-block">
+            <a class="btn d-flex align-items-center justify-content-between bg-primary w-100" data-toggle="collapse" href="#navbar-vertical" style="height: 65px; padding: 0 30px;">
+                <h6 class="text-dark m-0"><i class="fa fa-bars mr-2"></i>Categories</h6>
+                <i class="fa fa-angle-down text-dark"></i>
+            </a>
+            <nav class="collapse position-absolute navbar navbar-vertical navbar-light align-items-start p-0 bg-light" id="navbar-vertical" style="width: calc(100% - 30px); z-index: 999;">
+                <div class="navbar-nav w-100">
+                    <a href="shop1.php?Categories=1" class="nav-item nav-link">keyboard</a>
+                    <a href="shop1.php?Categories=2" class="nav-item nav-link">Gaming laptop</a>
+                    <a href="shop1.php?Categories=3" class="nav-item nav-link">mouse</a>
+                    <a href="shop1.php?Categories=4" class="nav-item nav-link">Gaming chair</a>
+                    <a href="shop1.php?Categories=5" class="nav-item nav-link">Gaming mic</a>
+                    <a href="shop1.php?Categories=6" class="nav-item nav-link">Joy stick & Console</a>
+                    <a href="shop1.php?Categories=7" class="nav-item nav-link">speaker</a>
+                    <a href="shop1.php?Categories=8" class="nav-item nav-link">Screen</a>
+                    <a href="shop1.php?Categories=9" class="nav-item nav-link">Earphones</a>
+                </div>
+            </nav>
+        </div>
+        <div class="col-lg-9">
+            <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 py-lg-0 px-0">
+                <a href="" class="text-decoration-none d-block d-lg-none">
+                    <span class="h1 text-uppercase text-dark bg-light px-2">Elite</span>
+                    <span class="h1 text-uppercase text-light bg-primary px-2 ml-n1">Pixel</span>
                 </a>
-                <nav class="collapse position-absolute navbar navbar-vertical navbar-light align-items-start p-0 bg-light" id="navbar-vertical" style="width: calc(100% - 30px); z-index: 999;">
-                    <div class="navbar-nav w-100">
-                        
-                        <a href="shop1.php?Categories=1" class="nav-item nav-link">keyboard</a>
-                        <a href="shop1.php?Categories=2" class="nav-item nav-link">Gaming laptop</a>
-                        <a href="shop1.php?Categories=3" class="nav-item nav-link">mouse</a>
-                        <a href="shop1.php?Categories=4" class="nav-item nav-link">Gaming chair</a>
-                        <a href="shop1.php?Categories=5" class="nav-item nav-link">Gaming mic</a>
-                        <a href="shop1.php?Categories=6" class="nav-item nav-link">Joy stick & Console</a>
-                        <a href="shop1.php?Categories=7" class="nav-item nav-link">speaker</a>
-                        <a href="shop1.php?Categories=8" class="nav-item nav-link">Screen</a>
-                        <a href="shop1.php?Categories=9" class="nav-item nav-link">Earphones</a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                    <div class="navbar-nav mr-auto py-0">
+                        <a href="index1.php" class="nav-item nav-link active">Home</a>
+                        <a href="shop.php" class="nav-item nav-link">Shop</a>
+                        <a href="contact1.php" class="nav-item nav-link">Contact</a>
                     </div>
-                </nav>
-            </div>
-            <div class="col-lg-9">
-                <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 py-lg-0 px-0">
-                    <a href="" class="text-decoration-none d-block d-lg-none">
-                        <span class="h1 text-uppercase text-dark bg-light px-2">Elite</span>
-                        <span class="h1 text-uppercase text-light bg-primary px-2 ml-n1">Pixel</span>
-                    </a>
-                    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-                        <div class="navbar-nav mr-auto py-0">
-                            <a href="index1.php" class="nav-item nav-link active">Home</a>
-                            <a href="shop.php" class="nav-item nav-link">Shop</a>
-                            
-                            
-                            <a href="contact1.php" class="nav-item nav-link">Contact</a>
-                        </div>
-                        <div class="navbar-nav ml-auto py-0 d-none d-lg-block">
-                            
-                            <a href="" class="btn px-0 ml-3">
-                                <i class="fas fa-shopping-cart text-primary"></i>
-                                <span class="badge text-secondary border border-secondary rounded-circle" style="padding-bottom: 2px;">0</span>
-                            </a>
-                        </div>
+                    <div class="navbar-nav ml-auto py-0 d-none d-lg-block">
+                        <a href="" class="btn px-0 ml-3">
+                            <i class="fas fa-shopping-cart text-primary"></i>
+                            <span class="badge text-secondary border border-secondary rounded-circle" style="padding-bottom: 2px;">0</span>
+                        </a>
                     </div>
-                </nav>
-            </div>
+                </div>
+            </nav>
         </div>
     </div>
-    <!-- Navbar End -->
+</div>
+<!-- Navbar End -->
 
-    <!-- Featured Start -->
-    <div class="container-fluid pt-5">
+<!-- Featured Start -->
+<div class="container-fluid pt-5">
     <div class="row px-xl-5 pb-3 justify-content-center">
         <div class="container-fluid pt-5 pb-3">
             <h2>ผลลัพธ์การค้นหา: "<?php echo htmlspecialchars($search_query); ?>"</h2>
@@ -249,7 +245,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         $imageSrc = "img/" . $iditem . ".1.jpg"; // กำหนดที่อยู่ไฟล์รูปตาม iditem
 
                         echo '
-                        <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
+                        <div class="col-lg-3 col-md-4 col-sm-6 mb-3"> <!-- ปรับระยะห่างระหว่างสินค้า -->
                             <div class="product-item bg-light mb-4">
                                 <div class="product-img position-relative overflow-hidden">
                                     <!-- แสดงรูปตาม iditem -->
@@ -263,12 +259,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                     <a class="h6 text-decoration-none text-truncate" href="detail1.php?Iditem=' . $iditem . '">' . $name . '</a>
                                     <div class="d-flex align-items-center justify-content-center mt-2">
                                         <h5 class="text-muted ml-2">$' . number_format($price, 2) . '</h5>
-
                                     </div>
                                     <div class="d-flex align-items-center justify-content-center mt-2">
                                         <h6 style="font-size: 12px; ">เหลือ ' . $num . ' ชิ้น</h6>
                                     </div>
-                                    
                                 </div>
                             </div>
                         </div>';
@@ -281,6 +275,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </div>
     </div>
 </div>
+<!-- Featured End -->
+
 
 
 
