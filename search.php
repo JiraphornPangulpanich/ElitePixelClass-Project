aaa6bfff07cbd8b67b5c159901d8952721a712fe
Jiraphorn4625 <65010914625@msu.ac.th>
2025-03-09 21:55:00 +0700

    s

--- search.php
+++ search.php
@@ -17,6 +17,49 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html lang="th">
 <head>
+<meta charset="utf-8">
+    <title> ElitePixel </title>
+    
+   
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">  
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+
+<style>
+ .bn1 {
+  transition-duration: 0.4s;
+}
+
+.bn1:hover {
+  background-color: #04AA6D; /* Green */
+  color: white;
+}
+</style>
+   
+    
+</head>
+
     <meta charset="UTF-8">
     <title>ผลการค้นหา</title>
     <link rel="stylesheet" href="styles.css">
