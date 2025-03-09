4c2ac9ff76b3798fc7188468aed04e830836b48e
Jiraphorn4625 <65010914625@msu.ac.th>
2025-03-10 00:50:28 +0700

    ห

--- cart1.php
+++ cart1.php
@@ -99,8 +99,8 @@ if (isset($_SESSION['cart']) && !empty($_SESSION['cart'])) {
             }
         } else {
             // ถ้าไม่มีสินค้าตรงกับ Iditem
-            echo "<li>ไม่พบข้อมูลสินค้าที่คุณเลือก</li>";
-            echo "<a href='cart1.php?action=remove&id=$itemId' class='btn btn-danger'>ลบออกจากตะกร้า</a>";
+            echo "<li>ไม่พบข้อมูลสินค้าที่คุณเลือก</li><a href='cart1.php?action=remove&id=$itemId' class='btn btn-danger'>ลบออกจากตะกร้า</a>";
+            
         }
     }
     echo "</ul>"; // ปิดรายการสินค้า
