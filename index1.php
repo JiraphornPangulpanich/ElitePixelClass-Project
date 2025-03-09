b0b36c9303c4f6fba45dd944820fcb547475a845
Jiraphorn4625 <65010914625@msu.ac.th>
2025-03-09 21:29:46 +0700

    s

--- index1.php
+++ index1.php
@@ -94,7 +94,7 @@ $fullname = $_SESSION["firstname"] . " " . $_SESSION["lastname"];
                 <form action="search.php" method="POST">
                     <div class="input-group">
                         <input type="text" class="form-control" name="seach_query" placeholder="Search for products">
-                        <button type="submit">🔍 ค้นหา</button>
+                        <button1 type="submit">🔍 ค้นหา</button1>
                         
                     </div>
                 </form>
