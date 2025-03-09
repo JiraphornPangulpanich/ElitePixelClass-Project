8d0254722e651f65ee85130626475d60b1723ed1
Jiraphorn4625 <65010914625@msu.ac.th>
2025-03-10 01:56:58 +0700

    ห

--- testphp.php
+++ testphp.php
@@ -249,158 +249,110 @@ if (isset($_SESSION['cart']) && !empty($_SESSION['cart'])) {
 ?>
     <!-- Cart Start -->
     <div class="container-fluid">
-        <div class="row px-xl-5">
-            <div class="col-lg-8 table-responsive mb-5">
-                <table class="table table-light table-borderless table-hover text-center mb-0">
-                    <thead class="thead-dark">
-                        <tr>
-                            <th>Products</th>
-                            <th>Price</th>
-                            <th>Quantity</th>
-                            <th>Total</th>
-                            <th>Remove</th>
-                        </tr>
-                    </thead>
-                    <tbody class="align-middle">
-                        <tr>
-                            <td class="align-middle"><img src="img/product-1.jpg" alt="" style="width: 50px;"> Product Name</td>
-                            <td class="align-middle">$150</td>
-                            <td class="align-middle">
-                                <div class="input-group quantity mx-auto" style="width: 100px;">
-                                    <div class="input-group-btn">
-                                        <button class="btn btn-sm btn-primary btn-minus" >
-                                        <i class="fa fa-minus"></i>
-                                        </button>
-                                    </div>
-                                    <input type="text" class="form-control form-control-sm bg-secondary border-0 text-center" value="1">
-                                    <div class="input-group-btn">
-                                        <button class="btn btn-sm btn-primary btn-plus">
-                                            <i class="fa fa-plus"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="align-middle">$150</td>
-                            <td class="align-middle"><button class="btn btn-sm btn-danger"><i class="fa fa-times"></i></button></td>
-                        </tr>
-                        <tr>
-                            <td class="align-middle"><img src="img/product-2.jpg" alt="" style="width: 50px;"> Product Name</td>
-                            <td class="align-middle">$150</td>
-                            <td class="align-middle">
-                                <div class="input-group quantity mx-auto" style="width: 100px;">
-                                    <div class="input-group-btn">
-                                        <button class="btn btn-sm btn-primary btn-minus" >
-                                        <i class="fa fa-minus"></i>
-                                        </button>
-                                    </div>
-                                    <input type="text" class="form-control form-control-sm bg-secondary border-0 text-center" value="1">
-                                    <div class="input-group-btn">
-                                        <button class="btn btn-sm btn-primary btn-plus">
-                                            <i class="fa fa-plus"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="align-middle">$150</td>
-                            <td class="align-middle"><button class="btn btn-sm btn-danger"><i class="fa fa-times"></i></button></td>
-                        </tr>
-                        <tr>
-                            <td class="align-middle"><img src="img/product-3.jpg" alt="" style="width: 50px;"> Product Name</td>
-                            <td class="align-middle">$150</td>
-                            <td class="align-middle">
-                                <div class="input-group quantity mx-auto" style="width: 100px;">
-                                    <div class="input-group-btn">
-                                        <button class="btn btn-sm btn-primary btn-minus" >
-                                        <i class="fa fa-minus"></i>
-                                        </button>
-                                    </div>
-                                    <input type="text" class="form-control form-control-sm bg-secondary border-0 text-center" value="1">
-                                    <div class="input-group-btn">
-                                        <button class="btn btn-sm btn-primary btn-plus">
-                                            <i class="fa fa-plus"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="align-middle">$150</td>
-                            <td class="align-middle"><button class="btn btn-sm btn-danger"><i class="fa fa-times"></i></button></td>
-                        </tr>
-                        <tr>
-                            <td class="align-middle"><img src="img/product-4.jpg" alt="" style="width: 50px;"> Product Name</td>
-                            <td class="align-middle">$150</td>
-                            <td class="align-middle">
-                                <div class="input-group quantity mx-auto" style="width: 100px;">
-                                    <div class="input-group-btn">
-                                        <button class="btn btn-sm btn-primary btn-minus" >
-                                        <i class="fa fa-minus"></i>
-                                        </button>
-                                    </div>
-                                    <input type="text" class="form-control form-control-sm bg-secondary border-0 text-center" value="1">
-                                    <div class="input-group-btn">
-                                        <button class="btn btn-sm btn-primary btn-plus">
-                                            <i class="fa fa-plus"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="align-middle">$150</td>
-                            <td class="align-middle"><button class="btn btn-sm btn-danger"><i class="fa fa-times"></i></button></td>
-                        </tr>
-                        <tr>
-                            <td class="align-middle"><img src="img/product-5.jpg" alt="" style="width: 50px;"> Product Name</td>
-                            <td class="align-middle">$150</td>
-                            <td class="align-middle">
-                                <div class="input-group quantity mx-auto" style="width: 100px;">
-                                    <div class="input-group-btn">
-                                        <button class="btn btn-sm btn-primary btn-minus" >
-                                        <i class="fa fa-minus"></i>
-                                        </button>
-                                    </div>
-                                    <input type="text" class="form-control form-control-sm bg-secondary border-0 text-center" value="1">
-                                    <div class="input-group-btn">
-                                        <button class="btn btn-sm btn-primary btn-plus">
-                                            <i class="fa fa-plus"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="align-middle">$150</td>
-                            <td class="align-middle"><button class="btn btn-sm btn-danger"><i class="fa fa-times"></i></button></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <div class="col-lg-4">
-                <form class="mb-30" action="">
-                    <div class="input-group">
-                        <input type="text" class="form-control border-0 p-4" placeholder="Coupon Code">
-                        <div class="input-group-append">
-                            <button class="btn btn-primary">Apply Coupon</button>
-                        </div>
-                    </div>
-                </form>
-                <h5 class="section-title position-relative text-uppercase mb-3"><span class="bg-secondary pr-3">Cart Summary</span></h5>
-                <div class="bg-light p-30 mb-5">
-                    <div class="border-bottom pb-2">
-                        <div class="d-flex justify-content-between mb-3">
-                            <h6>Subtotal</h6>
-                            <h6>$150</h6>
-                        </div>
-                        <div class="d-flex justify-content-between">
-                            <h6 class="font-weight-medium">Shipping</h6>
-                            <h6 class="font-weight-medium">$10</h6>
-                        </div>
-                    </div>
-                    <div class="pt-2">
-                        <div class="d-flex justify-content-between mt-2">
-                            <h5>Total</h5>
-                            <h5>$160</h5>
-                        </div>
-                        <button class="btn btn-block btn-primary font-weight-bold my-3 py-3">Proceed To Checkout</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <?php
+session_start();
+include "config.php"; // ไฟล์เชื่อมต่อฐานข้อมูล
+
+if (isset($_GET['action']) && isset($_GET['id'])) {
+    $itemId = $_GET['id'];
+    $action = $_GET['action'];
+
+    if (!isset($_SESSION['cart'])) {
+        $_SESSION['cart'] = [];
+    }
+
+    $sql = "SELECT Num FROM Product WHERE Iditem = '$itemId'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $availableQuantity = $row['Num'];
+
+        if ($action == 'add') {
+            if (isset($_SESSION['cart'][$itemId])) {
+                if ($_SESSION['cart'][$itemId] < $availableQuantity) {
+                    $_SESSION['cart'][$itemId]++;
+                    $conn->query("UPDATE Product SET Num = Num - 1 WHERE Iditem = '$itemId'");
+                } else {
+                    echo "<script>alert('สินค้ามีจำนวนไม่เพียงพอในคลัง');</script>";
+                }
+            } else {
+                $_SESSION['cart'][$itemId] = 1;
+                $conn->query("UPDATE Product SET Num = Num - 1 WHERE Iditem = '$itemId'");
+            }
+        }
+
+        if ($action == 'decrease' && isset($_SESSION['cart'][$itemId])) {
+            if ($_SESSION['cart'][$itemId] > 1) {
+                $_SESSION['cart'][$itemId]--;
+                $conn->query("UPDATE Product SET Num = Num + 1 WHERE Iditem = '$itemId'");
+            } else {
+                unset($_SESSION['cart'][$itemId]);
+            }
+        }
+
+        if ($action == 'remove' && isset($_SESSION['cart'][$itemId])) {
+            $conn->query("UPDATE Product SET Num = Num + {$_SESSION['cart'][$itemId]} WHERE Iditem = '$itemId'");
+            unset($_SESSION['cart'][$itemId]);
+        }
+    }
+}
+
+// แสดงสินค้าในตะกร้า
+echo '<div class="row px-xl-5">';
+echo '<div class="col-lg-8 table-responsive mb-5">';
+echo '<table class="table table-light table-borderless table-hover text-center mb-0">';
+echo '<thead class="thead-dark"><tr><th>Products</th><th>Price</th><th>Quantity</th><th>Total</th><th>Remove</th></tr></thead>';
+echo '<tbody class="align-middle">';
+
+$totalPrice = 0;
+
+if (!empty($_SESSION['cart'])) {
+    foreach ($_SESSION['cart'] as $itemId => $quantity) {
+        $sql = "SELECT Name, Price, Image FROM Product WHERE Iditem = '$itemId'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $price = $row['Price'];
+            $total = $price * $quantity;
+            $totalPrice += $total;
+            
+            echo '<tr>';
+            echo '<td class="align-middle"><img src="' . $row['Image'] . '" style="width: 50px;"> ' . $row['Name'] . '</td>';
+            echo '<td class="align-middle">$' . number_format($price, 2) . '</td>';
+            echo '<td class="align-middle">';
+            echo '<div class="input-group quantity mx-auto" style="width: 100px;">';
+            echo '<div class="input-group-btn">';
+            echo '<a href="cart1.php?action=decrease&id=' . $itemId . '" class="btn btn-sm btn-primary btn-minus"><i class="fa fa-minus"></i></a>';
+            echo '</div>';
+            echo '<input type="text" class="form-control form-control-sm bg-secondary border-0 text-center" value="' . $quantity . '" readonly>';
+            echo '<div class="input-group-btn">';
+            echo '<a href="cart1.php?action=add&id=' . $itemId . '" class="btn btn-sm btn-primary btn-plus"><i class="fa fa-plus"></i></a>';
+            echo '</div>';
+            echo '</div>';
+            echo '</td>';
+            echo '<td class="align-middle">$' . number_format($total, 2) . '</td>';
+            echo '<td class="align-middle"><a href="cart1.php?action=remove&id=' . $itemId . '" class="btn btn-sm btn-danger"><i class="fa fa-times"></i></a></td>';
+            echo '</tr>';
+        }
+    }
+} else {
+    echo '<tr><td colspan="5">ตะกร้าของคุณยังว่างอยู่</td></tr>';
+}
+
+echo '</tbody></table></div>';
+
+echo '<div class="col-lg-4">';
+echo '<h5 class="section-title position-relative text-uppercase mb-3"><span class="bg-secondary pr-3">Cart Summary</span></h5>';
+echo '<div class="bg-light p-30 mb-5">';
+echo '<div class="d-flex justify-content-between mb-3"><h6>Subtotal</h6><h6>$' . number_format($totalPrice, 2) . '</h6></div>';
+echo '<div class="d-flex justify-content-between"><h6 class="font-weight-medium">Shipping</h6><h6 class="font-weight-medium">$10</h6></div>';
+echo '<div class="d-flex justify-content-between mt-2"><h5>Total</h5><h5>$' . number_format($totalPrice + 10, 2) . '</h5></div>';
+echo '<button class="btn btn-block btn-primary font-weight-bold my-3 py-3">Proceed To Checkout</button>';
+echo '</div></div></div>';
+?>
+
     </div>
 
     <!-- Cart End -->
