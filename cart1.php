c2bdff3aba25002b921d8ba29e2406c531c9a53b
Jiraphorn4625 <65010914625@msu.ac.th>
2025-03-09 23:48:52 +0700

    s

--- cart1.php
+++ cart1.php
@@ -1,16 +1,11 @@
 <?php
 session_start(); // เริ่มต้น session
-
-// ตรวจสอบค่าของ $_SESSION['cart']
-echo '<pre>';
-print_r($_SESSION['cart']);
-echo '</pre>';
-
-if (!isset($_SESSION['cart']) || empty($_SESSION['cart'])) {
-    echo "<h2>ตะกร้าของคุณยังว่างอยู่</h2>";
-} else {
+if (isset($_SESSION['cart']) && !empty($_SESSION['cart'])) {
     echo "<h2>สินค้าที่อยู่ในตะกร้าของคุณ</h2>";
-    // ส่วนแสดงข้อมูลสินค้าจากตะกร้า
+    echo '<pre>';
+    print_r($_SESSION['cart']); // แสดงสินค้าทั้งหมดในตะกร้า
+    echo '</pre>';
+} else {
+    echo "<h2>ตะกร้าของคุณยังว่างอยู่</h2>";
 }
-
 ?>
