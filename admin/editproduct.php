d5377110986f80db93182176fa74fef81af317e6
Jiraphorn4625 <65010914625@msu.ac.th>
2025-03-10 13:25:12 +0000

    a

--- admin/editproduct.php
+++ admin/editproduct.php
@@ -88,5 +88,13 @@ if (isset($_POST['submit'])) {
             <a href="product.php" class="btn btn-secondary">กลับหน้าเดิม</a>
         </form>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/chart-area-demo.js"></script>
+        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+        <script src="js/datatables-simple-demo.js"></script>
 </body>
 </html>
