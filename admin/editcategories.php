ed01788e55d8d0b22ed53b840f829b9cf19a5a9e
Jiraphorn4625 <65010914625@msu.ac.th>
2025-03-10 18:27:43 +0000

    ฟ

--- admin/editcategories.php
+++ admin/editcategories.php
@@ -1,7 +1,7 @@
 <?php
 include 'condb.php';
 
-
+$id = $_GET['id'];
 
 // ดึงข้อมูลหมวดหมู่
 $sql = "SELECT * FROM Categories WHERE id = '$id'";
@@ -58,5 +58,11 @@ if (isset($_POST['submit'])) {
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/chart-area-demo.js"></script>
+        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+        <script src="js/datatables-simple-demo.js"></script>
 </body>
 </html>
