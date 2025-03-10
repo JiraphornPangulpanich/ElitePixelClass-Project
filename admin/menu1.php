f67d0825025ac575a72fb557c839ba51eba71e90
Jiraphorn4625 <65010914625@msu.ac.th>
2025-03-10 14:03:51 +0000

    ฟ

--- admin/menu1.php
+++ admin/menu1.php
@@ -36,13 +36,15 @@
                             <div class="sb-sidenav-menu-heading">Interface</div>
                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                จัดการสินค้า
+                                จัดการร้าน
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
                             <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
                                     <a class="nav-link" href="product.php">สินค้าทั้งหมด</a>
                                     <a class="nav-link" href="categories.php">ประเภทสินค้า</a>
+                                    <a class="nav-link" href=".php">ข้อมูลลูกค้า</a>
+                                    <a class="nav-link" href=".php">รายการสั่งซื้อ</a>
                                 </nav>
                             </div>
                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
